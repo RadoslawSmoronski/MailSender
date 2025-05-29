c16d4a960f7149e4d6f173b552fd3667be2059eb
RadoslawSmoronski <radoslaw.smo@gmail.com>
2025-05-29 13:11:44 +0200

    Create SmtpService

---
+++ MailSender.Application/Services/Interfaces/ISmtpService.cs
@@ -0,0 +1,15 @@
+﻿using MailSender.Common.Result;
+using MailSender.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender.Application.Services.Interfaces
+{
+    public interface ISmtpService
+    {
+        Result Send(MailDto mailDto, SmtpDto smtpDto);
+    }
+}
---
+++ MailSender.Application/Services/SmtpService.cs
@@ -0,0 +1,42 @@
+﻿using MailKit.Security;
+using MailSender.Application.Services.Interfaces;
+using MailSender.Common.Result;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+using MailSender.Domain.DTOs;
+
+namespace MailSender.Application.Services
+{
+    public class SmtpService : ISmtpService
+    {
+        public Result Send(MailDto mailDto, SmtpDto smtpDto)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(smtpDto.User, smtpDto.User));
+            message.To.Add(new MailboxAddress(mailDto.To, mailDto.To));
+            message.Subject = mailDto.Subject;
+
+            message.Body = new TextPart("html")
+            {
+                Text = mailDto.Body
+            };
+
+            using var client = new SmtpClient();
+
+            client.ServerCertificateValidationCallback = (sender, certificate, chain, errors) =>
+            {
+                return true;
+            };
+
+            client.Connect(smtpDto.Host, smtpDto.Port, SecureSocketOptions.StartTls);
+
+            client.Authenticate(smtpDto.User, smtpDto.Password);
+
+            client.Send(message);
+            client.Disconnect(true);
+
+            return Result.Success();
+        }
+    }
+}
